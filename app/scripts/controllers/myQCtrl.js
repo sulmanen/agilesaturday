a8063c0ae54eaced663f7805ae3e6bab7710ac8e
Samuli Ulmanen <samuli.ulmanen@rovio.com>
2013-09-28 15:34:43 +0300

    add throttling

--- app/scripts/controllers/myQCtrl.js
+++ app/scripts/controllers/myQCtrl.js
@@ -1,12 +1,24 @@
 'use strict';
 
 angular.module('asApp')
-.controller('myQCtrl', ['$scope', '$resource', 
-	function ($scope, $resource) {
-   	$scope.myQCtrl = {
-   		search: function(query) {
-   			return $resource('http://localhost\\:8000/q/:q')
-   			.get({q: query});
-   		}
-   	}
-}]);
+    .controller('myQCtrl', ['$scope', '$resource', '$timeout',
+        function($scope, $resource, $timeout) {
+            var queryTimeout;
+            var tempQuery;
+            $scope.myQCtrl = {
+            	tweets: undefined,
+                search: function(query) {
+                    if (queryTimeout) {
+                        $timeout.cancel(queryTimeout);
+                    }
+                    tempQuery = query;
+                    queryTimeout = $timeout(function() {
+                        $scope.myQCtrl.tweets = $resource('http://localhost\\:8000/q/:q')
+                            .get({
+                                q: query
+                            });
+                    }, 1000);
+                }
+            }
+        }
+    ]);
